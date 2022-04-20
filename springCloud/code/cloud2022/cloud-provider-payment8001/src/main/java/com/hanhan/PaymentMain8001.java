7f628b7fd472080496f11794684152e94a66b1bd
hanhan12 <1607077440@qq.com>
2022-04-20 23:19:53 +0800

    123

---
+++ springCloud/code/cloud2022/cloud-provider-payment8001/src/main/java/com/hanhan/PaymentMain8001.java
@@ -0,0 +1,14 @@
+package com.hanhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hanhan12
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
