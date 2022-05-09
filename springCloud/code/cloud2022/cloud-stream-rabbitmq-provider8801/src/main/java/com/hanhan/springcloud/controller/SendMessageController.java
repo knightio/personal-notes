a340625870acdacc3a525cfc57c2d086eb131279
knightio <1607077440@qq.com>
2022-05-09 17:45:03 +0800

    123

---
+++ springCloud/code/cloud2022/cloud-stream-rabbitmq-provider8801/src/main/java/com/hanhan/springcloud/controller/SendMessageController.java
@@ -0,0 +1,20 @@
+package com.hanhan.springcloud.controller;
+
+import com.hanhan.springcloud.service.IMessageProvider;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class SendMessageController {
+
+    @Resource
+    private IMessageProvider messageProvider;
+
+    @GetMapping("/sendMessage")
+    public String sendMessage(){
+        return messageProvider.send();
+    }
+
+}
